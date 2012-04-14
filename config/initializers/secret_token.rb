b38cbbe32044b2a1c99cabea2bb171f7e3e882da
Ola Loevholm <ola@lovholm.net>
2012-04-14 16:07:56 +0200

    First BackEnd commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelseBackEnd::Application.config.secret_token = '0f29d9f9e4cdc5fbb9fee083a211bb559a796dc4b1f6e74e9cda211e1b5727056d5521f2870a013b3117cc5b282b703c88bab1b789fee70b05c23a6d4c9a27aa'
