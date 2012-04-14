c6130dff6043e5d62d783578b8a563dfcf1fc6e9
Ola Loevholm <ola@lovholm.net>
2012-04-14 18:10:58 +0200

    new and create for employee produced

--- app/controllers/employee_controller.rb
+++ app/controllers/employee_controller.rb
@@ -16,12 +16,17 @@ class EmployeeController < ApplicationController
   
   def create
      @employee = Employee.new(params[:employee])
+     if params[:employee]['woman'] == 'false'
+       @employee.woman = false
+      else 
+        @employee.woman = true
+      end
      if @employee.save
        flash[:success] = "Brukeren #{@employee.firstname} ble opprettet"
-       redirect_to :action => list
+       redirect_to :action => 'list'
      else 
        flash[:error] = "Brukeren kunne ikke bli opprettet. Forsøk igjen"
-       render :action => :new
+       render :action => 'new'
     end
     
   end 
