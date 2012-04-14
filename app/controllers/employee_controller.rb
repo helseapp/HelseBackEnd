6ddb7a8206b0a7de785338e49f0b714acbf139fc
Ola Loevholm <ola@lovholm.net>
2012-04-14 17:45:49 +0200

    Chchchchanges

--- app/controllers/employee_controller.rb
+++ app/controllers/employee_controller.rb
@@ -1,2 +1,43 @@
 class EmployeeController < ApplicationController
+  
+  def list 
+    @employees = Employee.all
+    
+  end 
+  
+  def show
+    
+  end 
+  
+  def new 
+    @employee = Employee.new
+    
+  end 
+  
+  def create
+     @employee = Employee.new(params[:employee])
+     if @employee.save
+       flash[:success] = "Brukeren #{@employee.firstname} ble opprettet"
+       redirect_to :action => list
+     else 
+       flash[:error] = "Brukeren kunne ikke bli opprettet. Forsøk igjen"
+       render :action => :new
+    end
+    
+  end 
+  
+  def update
+    
+  end
+  
+  def delete
+    
+  end
+
+
+  def index
+    redirect_to :action => "list"
+    
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,7 @@ HelseBackEnd::Application.routes.draw do
   #       get 'sold'
   #     end
   #   end
+  
 
   # Sample resource route with sub-resources:
   #   resources :products do
@@ -54,5 +55,11 @@ HelseBackEnd::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
+  
+  #POST REQUST ROUTES
+  match "employee/create" => "employee#create"
+  
+  
+  
+  match ':controller(/:action(/:id))(.:format)'
 end
