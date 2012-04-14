aaaf53f67efc43e4b07700639375e4e8cb681199
Ola Loevholm <ola@lovholm.net>
2012-04-14 19:32:48 +0200

    workplace#show created as a table (just temporarily)

--- app/controllers/workplace_controller.rb
+++ app/controllers/workplace_controller.rb
@@ -5,15 +5,26 @@ class WorkplaceController < ApplicationController
   end
   
   def show
+    @workplace = Workplace.find(params[:id])
     
   end
   
   def new
+    @workplace.new
     
   end 
   
   def create
+    @workplace.new(:params[:workplace])
     
+  end
+  
+  def update 
+    
+  end
+  
+  def delete 
+  
   end
   
   
