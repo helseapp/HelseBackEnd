6e2f3bf6de89e0ad59088f894a1d59670d041ef6
Ola Loevholm <ola@lovholm.net>
2012-04-23 18:21:01 +0200

    new commit

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,5 @@ class Task < ActiveRecord::Base
   # attr_accessible :title, :body
   belongs_to :task_template
   belongs_to :visit
+
 end
--- db/migrate/20120416153935_create_appusers.rb
+++ db/migrate/20120416153935_create_appusers.rb
@@ -3,6 +3,7 @@ class CreateAppusers < ActiveRecord::Migration
   def change
     create_table :appusers do |t|    
       t.references :employee
+      t.boolean :is_admin, :default => false
       
       t.string :hashed_password
 
