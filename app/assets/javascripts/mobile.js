dd044356f3e0e9df78438bac6ddcdac0e8780a34
Kat Aquino <katfaquino@gmail.com>
2012-04-23 22:22:08 +0200

    up and down scrollers

--- app/assets/javascripts/mobile.js
+++ app/assets/javascripts/mobile.js
@@ -3,25 +3,41 @@
 // You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
 
 $(document).ready(function(){
+    // Show/hide more info about the patient
     $('#more_info').click(function(){
         if($('#more_info').attr('src') === "/assets/more.png"){
             $('#more_info').attr("src","/assets/less.png");
-            $('#extended_info').show("slow");
+            $('#extended_info').slideDown("slow");
         }
         else{
             $('#more_info').attr('src', '/assets/more.png');
-            $('#extended_info').stop().hide("slow");
+            $('#extended_info').stop().slideUp("slow");
         }   
     }),
     
+    // Show/hide task descriptions
     $('.tasks').click(function(){
         id = $(this).attr('id');
 
         if($('#task_description' + id).css('display') === "none"){
-            $('#task_description' + id).show('slow');
+            $('#task_description' + id).slideDown('slow');
         }
         else{
-            $('#task_description'+id).stop().hide('slow');
+            $('#task_description'+id).stop().slideUp('slow');
         }
+    }),
+    
+    // Scroll up and down
+    $('#up').click(function(){
+        scroll = $('.screen').scrollTop();
+        $('.screen').animate({'scrollTop': scroll-100}, 1000);
+    }),
+    
+    $('#down').click(function(){
+        scroll = $('.screen').scrollTop();
+        $('.screen').animate({'scrollTop': scroll+100}, 1000);
+    
     })
+    
+    
 });
